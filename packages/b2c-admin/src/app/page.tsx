6de2e55c7844dc364a4fffc8bd961af5062d3c1b
hieudd <hieuduong@asmingroup.vn>
2024-12-05 11:02:40 +0700

    Create codebase

---
+++ packages/b2c-admin/src/app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { db } from 'common/libs/db';
+const HomePage = async () => {
+  const data = await db.user.findMany();
+
+  return (
+    <div>
+      {data?.map((user) => (
+        <p key={user.id}>{user.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default HomePage;
