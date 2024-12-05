f4d659c3d5539934b9a6a70d077cb54a780a027e
hieudd <hieuduong@asmingroup.vn>
2024-12-05 11:26:07 +0700

    Add eslint and husky pre-commit

--- packages/b2c-admin/src/app/page.tsx
+++ packages/b2c-admin/src/app/page.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import { db } from 'common/libs/db';
+
 const HomePage = async () => {
   const data = await db.user.findMany();
 
-  return (
-    <div>
-      {data?.map((user) => (
-        <p key={user.id}>{user.name}</p>
-      ))}
-    </div>
-  );
+  return <div>{data?.map((user) => <p key={user.id}>{user.name}</p>)}</div>;
 };
 
 export default HomePage;
